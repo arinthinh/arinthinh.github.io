181669b7030fcfaa3641ae89fda3aee0cf7c15f1
arin <phuccthiinh@gmail.com>
2026-07-04 22:37:04 +0700

    update

--- script.js
+++ script.js
@@ -11,3 +11,27 @@ if (projectsSection) {
 
     sectionObserver.observe(projectsSection);
 }
+
+const projectModal = document.getElementById('project-modal');
+const projectModalBody = projectModal?.querySelector('.project-modal-body');
+const projectModalClose = projectModal?.querySelector('.project-modal-close');
+
+document.querySelectorAll('.project-trigger').forEach((trigger) => {
+    trigger.addEventListener('click', () => {
+        const template = trigger.nextElementSibling;
+        const icon = trigger.querySelector('img');
+        if (!projectModal || !projectModalBody || !(template instanceof HTMLTemplateElement)) return;
+
+        const content = template.content.cloneNode(true);
+        if (icon) content.prepend(icon.cloneNode(true));
+
+        projectModalBody.replaceChildren(content);
+        projectModal.showModal();
+    });
+});
+
+projectModalClose?.addEventListener('click', () => projectModal.close());
+
+projectModal?.addEventListener('click', (event) => {
+    if (event.target === projectModal) projectModal.close();
+});
